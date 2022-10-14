ade93e2d8e22550f01a2e9fd41711859706acfd8
Venika Sem <venika.sem@mths.ca>
2022-10-14 14:35:17 +0000

    changed init

---
+++ add_decimals.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Venika Sem
+# Created on: oct 2022
+# This program adds two decimals
+
+import math
+
+
+def main():
+    # this function adds two decimals
+
+    # input
+    first_decimal = 0.1
+    second_decimal = 0.2
+
+    # process
+    sum_of_decimals = first_decimal + second_decimal
+
+    # output
+    print("")
+    print("{0} + {1} = {2}.".format(first_decimal, second_decimal, sum_of_decimals))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
